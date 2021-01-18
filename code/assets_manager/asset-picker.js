55425da03a8e2c9fd8a65c2fe2de7981c29faeda
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2021-01-18 00:17:03 -0500

    Use new gif.

--- code/assets_manager/asset-picker.js
+++ code/assets_manager/asset-picker.js
@@ -7,17 +7,26 @@ var assetMenuApp = new Vue({
       <div>
         <button class="preview-button"
                 v-on:click="addAsset('palm_tree')">
-          <img src="../assets/palm_tree/preview.png">
+          <img src="../assets/palm_tree/preview/preview.png"
+               v-on:mouseenter="mouseEnter"
+               v-on:mouseleave="mouseLeave">
           Add palm tree
         </button>
       </div>
     </div>
   `,
   data: {
+    imageIndex: 0
   },
   methods: {
     addAsset(assetName){
       window.addAsset(assetName);
     },
+    mouseEnter(e){
+      e.target.src = "../assets/palm_tree/preview/preview.gif";
+    },
+    mouseLeave(e){
+      e.target.src = "../assets/palm_tree/preview/preview.png";
+    }
   }
 });
